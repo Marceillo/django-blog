2f20b0687bd1240bea49d8f3f46cc22e955dba9d
Marceillo De Wet <marceillo@hotmail.com>
2024-06-11 13:22:02 +0000

    add env.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-__cgaxrr^5#7l8(sw2_#9bj3poxhb6hwf%035=+9=nd)3y$uc-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-marceillo-djangoblog-ppsno9fl9pw.ws.codeinstitute-ide.net",".herokuapp.com"]
 
