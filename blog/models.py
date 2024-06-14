6b292fe886233c992c360602cadb1a3681def83a
Marceillo De Wet <marceillo@hotmail.com>
2024-06-14 07:13:43 +0000

    Migrate the comment ERD and add to the admin file

--- blog/models.py
+++ blog/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
 
     
 class comment(models.Model):
-    post = models.ForeignKey(post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(post, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
