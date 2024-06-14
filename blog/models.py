c7e8bd0a1a22796aefcf04828b682475b8c12db8
Marceillo De Wet <marceillo@hotmail.com>
2024-06-14 10:56:01 +0000

    clear errors in migration

--- blog/models.py
+++ blog/models.py
@@ -13,9 +13,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-
-
-        # ...
+        
 
     class Meta:
         ordering = ["-created_on"]
@@ -30,3 +28,12 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+   
+    
+
+
+class Meta:
+        ordering = ["created_on"]
+
+def __str__(self):
+    return f"Comment {self.body} by {self.author}"
