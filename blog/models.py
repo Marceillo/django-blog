c7495fc981bf01df4ee3c989cdc6e9ffe46bb18f
Marceillo De Wet <marceillo@hotmail.com>
2024-06-14 12:03:10 +0000

    Comment error repaired

--- blog/models.py
+++ blog/models.py
@@ -24,16 +24,15 @@ class Post(models.Model):
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commenter")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
    
-    
 
+    class Meta:
+        ordering = ["-created_on"]
 
-class Meta:
-        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} by {self.author}"
 
-def __str__(self):
-    return f"Comment {self.body} by {self.author}"
