e51603b69bfd37b2f71da53682ce1dae1acd26d5
Marceillo De Wet <marceillo@hotmail.com>
2024-06-17 08:07:51 +0000

    Add filter for to see posts with publisher status

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    model = Post
+    # to filter the posts with the status of Publisher
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
+    #queryset = Post.objects.all().order_by("created_on")
+    #queryset = Post.objects.all().order_by("-created_on") reverses the order
+    #queryset = Post.objects.filter(author=2)
+     # template_name = "post_list.html"
 
 
