59c6299132bacfb61d3efed761f3962064cd9737
Marceillo De Wet <marceillo@hotmail.com>
2024-06-19 08:09:04 +0000

    enable serving of static files git push origin main

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 #SECRET_KEY = 'django-insecure-__cgaxrr^5#7l8(sw2_#9bj3poxhb6hwf%035=+9=nd)3y$uc-'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-marceillo-djangoblog-ppsno9fl9pw.ws.codeinstitute-ide.net",".herokuapp.com"]
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
