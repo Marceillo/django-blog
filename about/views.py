06345db8593407bdfa41cdc0c739ef800a28fa5f
Marceillo De Wet <marceillo@hotmail.com>
2024-06-24 12:08:00 +0000

    add a fully-functioning collaboration form

--- about/views.py
+++ about/views.py
@@ -8,9 +8,16 @@ def about_me(request):
     """
     Renders the about page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            message.add_message(request, message.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
     
     about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm    
+    collaborate_form = CollaborateForm
+
     return render(request,"about/about.html",{"about": about,
     "collaborate_form": collaborate_form
     },
