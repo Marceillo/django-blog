3307d99c9a77a878bdb7c7b22afd08a81879f560
Marceillo De Wet <marceillo@hotmail.com>
2024-06-24 09:14:52 +0000

    Add a test print code to debug a error

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,8 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        #print request from the challenge
+        #print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -41,8 +43,12 @@ def post_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
+
+               
     
     comment_form = CommentForm()
+    #print request from the challenge
+    #print("Accessing post_detail view")
 
     return render(
         request,
