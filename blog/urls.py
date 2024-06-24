53963cd692954da795d77166cb7fc9e1d8c179bb
Marceillo De Wet <marceillo@hotmail.com>
2024-06-24 14:20:42 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('detail/<slug:slug>/', views.post_detail, name='post_detail'),
-       
-]
+    path('detail/<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('detail/<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+]   
